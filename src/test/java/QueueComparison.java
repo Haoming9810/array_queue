0d26a499e9a2dd51caed484a44599985b0867d76
Peng Du <rick.peng.du@gmail.com>
2020-10-14 17:14:52 -0700

    add code-along challenges

--- src/test/java/QueueComparison.java
+++ src/test/java/QueueComparison.java
@@ -11,6 +11,14 @@ public class QueueComparison {
 
     @Test
     public void usingBothQueues() {
+        // 500 queues, half are ArrayQueues, half ListQueues
+
+        // fill all 500 queues in one loop
+
+        // increment all values in the 500 queues by 1 in one loop
+
+        // verify all queues have the correct values
+
     }
 
     private void incrementQueueByOne(Queue<Integer> queue) {
